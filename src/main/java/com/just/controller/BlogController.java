641c01084fbdc9be5121aaff56ebb67e4b587587
Your Name <1240201765@qq.com>
2020-11-21 19:40:38 +0800

    quickstart

--- src/main/java/com/just/controller/BlogController.java
+++ src/main/java/com/just/controller/BlogController.java
@@ -2,14 +2,11 @@ package com.just.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BlogController {
-    @GetMapping("/hello")
-    @ResponseBody
-    public String greeting(@RequestParam("name") String name){
-        return "hello "+name;
+    @GetMapping("/")
+    public String greeting(){
+        return "index";
     }
 }
