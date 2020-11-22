2343842c17faace3aaffe310eaf0be99a752df1d
Your Name <1240201765@qq.com>
2020-11-22 16:25:04 +0800

    flyway

--- src/main/java/com/just/controller/BlogController.java
+++ src/main/java/com/just/controller/BlogController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
-    @GetMapping("/")
+    @GetMapping("/ot")
     public String greeting(){
         return "index";
     }
---
+++ src/main/java/com/just/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.just.controller;
+
+import com.just.mapper.UserMapper;
+import com.just.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+    @Resource
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+        return "index";
+    }
+}
